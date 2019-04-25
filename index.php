117024a95ceb2f2ea9e72da6f3f181d8f95db6d5
tommy-urban-media <tommy.krueger@urban-media.com>
2019-04-25 16:07:00 +0200

    fix gitignore

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: t_krueger
+	 * Date: 25.04.2019
+	 * Time: 16:01
+	 */
--- wp-content/plugins/sem-author-image/sem-author-image-info.php
+++
@@ -1,4 +0,0 @@
-<?php
-__('Author Image', 'sem-author-image');
-__('Add an image for an Author(s) on your site, which individual users can configure by editing their profile.', 'sem-author-image');
-?>
